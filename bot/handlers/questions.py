218135d75af411626d4b56e3262ce43a5c845207
Alpha787 <vasya22668@gmail.com>
2022-10-14 23:56:03 +0300

    Write response routers2

--- bot/handlers/questions.py
+++ bot/handlers/questions.py
@@ -1,11 +1,18 @@
 import logging
-from aiogram import Router, F
+from unicodedata import name
+from aiogram import Router, F, html
 from aiogram.filters import Text, Command
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from bot.keyboards.reply import rasp_keyboard
 
 router = Router()
+
+class Form(StatesGroup):
+    name = State()
+    like_bots = State()
+    language = State()
 
 @router.message(Command(commands="start"))
 async def cmd_help(message: Message):
@@ -32,11 +39,29 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     )
 
 @router.message(Text(text="Расписание", ignore_case=True))
-async def raspisanie(message: Message):
+async def raspisanie(message: Message, state: FSMContext):
+    await state.set_state(Form.name)
     await message.answer(
         "Представьтесь.\nПример: Фамилия И.О\n"
         "Это нужно для правильного получения расписания",
         reply_markup=ReplyKeyboardRemove()
+    )
+
+@router.message(Form.name)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
+    await state.set_state(Form.like_bots)
+    await message.answer(
+        f"Nice to meet you, {html.quote(message.text)}!\nDid you like it>",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Yes"),
+                    KeyboardButton(text="No"),
+                ]
+            ],
+            resize_keyboard=True
+        ),
     )
 
 @router.message(Text(text="Группа", ignore_case=True))
